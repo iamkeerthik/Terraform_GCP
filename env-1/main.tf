4170ec9d71052ea453895df9b3cee546bf0a55e8
Keerthik Shenoy <keerthik9879@gmail.com>
2023-06-20 17:40:28 +0530

    grafana added

--- env-1/main.tf
+++ env-1/main.tf
@@ -19,17 +19,17 @@ module "iam-service_account" {
   
 }
 
-# module "compute-engine" {
-#     source = "../modules/compute-engine"
-#     depends_on = [
-#       module.iam-service_account
-#     ]
-#     gcp_project = var.gcp_project
-#     name = var.name
-#     instance_type = var.instance_type
-#     instance_zone = var.instance_zone
-#     pluto_script  = file("pluto-userdata.ps1")
-# }
+module "compute-engine" {
+    source = "../modules/compute-engine"
+    depends_on = [
+      module.iam-service_account
+    ]
+    gcp_project = var.gcp_project
+    name = var.name
+    instance_type = var.instance_type
+    instance_zone = var.instance_zone
+    pluto_script  = file("pluto-userdata.ps1")
+}
 
 module "gke_cluster" {
   source = "../modules/gke"
@@ -46,36 +46,36 @@ module "gke_cluster" {
   location = var.location
 }
 
-# # module "grafana" {
-# #   source = "../modules/gke-extra"
-# # }
-# module "pub-sub" {
-#   source = "../modules/pub-sub"
-#   depends_on = [
-#     module.vpc
-#   ]
-#   topics = var.topics
-#   subscriptions = var.subscriptions
+# module "grafana" {
+#   source = "../modules/gke-extra"
 # }
+module "pub-sub" {
+  source = "../modules/pub-sub"
+  depends_on = [
+    module.vpc
+  ]
+  topics = var.topics
+  subscriptions = var.subscriptions
+}
 
-# module "sql" {
-#   source = "../modules/SQL"
-#   depends_on = [
-#     module.vpc
-#   ]
-#   name              = var.name
-#   gcp_project       = var.gcp_project
-#   region            = var.region
-#   db_tier = var.db_tier
-# }
+module "sql" {
+  source = "../modules/SQL"
+  depends_on = [
+    module.vpc
+  ]
+  name              = var.name
+  gcp_project       = var.gcp_project
+  region            = var.region
+  db_tier = var.db_tier
+}
 
-# module "monitoring" {
-#   source = "../modules/monitor"
-#   depends_on = [
-#     module.compute-engine,
-#     module.sql
-#   ]
-#   name = var.name
-#   instance_zone = var.instance_zone
-#   gcp_project =var.gcp_project
-# }
+module "monitoring" {
+  source = "../modules/monitor"
+  depends_on = [
+    module.compute-engine,
+    module.sql
+  ]
+  name = var.name
+  instance_zone = var.instance_zone
+  gcp_project =var.gcp_project
+}
